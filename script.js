785788d5c6ea2b76efee03a65b5899d357a78d61
hamzaatalif <hamzaatalif@gmail.com>
2021-10-03 03:36:26 +0500

    html refined 1.1

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// function inventar(){
-//     var a = document.getElementById("inventar");
-//     a.classList.add("active");
-// }
 document.getElementById("first").style.display = "block";
 document.getElementById("second").style.display = "none";
 document.getElementById("third").style.display = "none";
@@ -14,7 +10,7 @@ const Geldbeutel = document.getElementById("Geldbeutel");
 const Klamotten = document.getElementById("Klamotten");
 const Rucksack = document.getElementById("Rucksack");
 
-inventar.onclick = ()=>{
+inventar.onclick = () => {
     inventar.classList.add("active"); //show info box
     Schlüsselbund.classList.remove("active");
     Geldbeutel.classList.remove("active");
@@ -26,7 +22,7 @@ inventar.onclick = ()=>{
     document.getElementById("forth").style.display = "none";
     document.getElementById("fifth").style.display = "none";
 }
-Schlüsselbund.onclick = ()=>{
+Schlüsselbund.onclick = () => {
     inventar.classList.remove("active"); //show info box
     Schlüsselbund.classList.add("active");
     Geldbeutel.classList.remove("active");
@@ -38,7 +34,7 @@ Schlüsselbund.onclick = ()=>{
     document.getElementById("forth").style.display = "none";
     document.getElementById("fifth").style.display = "none";
 }
-Geldbeutel.onclick = ()=>{
+Geldbeutel.onclick = () => {
     inventar.classList.remove("active"); //show info box
     Schlüsselbund.classList.remove("active");
     Geldbeutel.classList.add("active");
@@ -50,7 +46,7 @@ Geldbeutel.onclick = ()=>{
     document.getElementById("forth").style.display = "none";
     document.getElementById("fifth").style.display = "none";
 }
-Klamotten.onclick = ()=>{
+Klamotten.onclick = () => {
     inventar.classList.remove("active"); //show info box
     Schlüsselbund.classList.remove("active");
     Geldbeutel.classList.remove("active");
@@ -62,7 +58,7 @@ Klamotten.onclick = ()=>{
     document.getElementById("forth").style.display = "block";
     document.getElementById("fifth").style.display = "none";
 }
-Rucksack.onclick = ()=>{
+Rucksack.onclick = () => {
     inventar.classList.remove("active"); //show info box
     Schlüsselbund.classList.remove("active");
     Geldbeutel.classList.remove("active");
@@ -87,10 +83,10 @@ const trinken = document.getElementById("trinken");
 const munition = document.getElementById("munition");
 const sonstiges = document.getElementById("sonstiges");
 
-alles.onclick = ()=>{
+alles.onclick = () => {
     alles.style.background = "rgba(255, 255, 255, 0.301)";
     alles.style.background = "linear-gradient(0deg, rgba(255,255,255,0.2) 0%, rgba(255,255,255,0.4) 35%, rgba(255,255,255,0.4) 72%, rgba(255,255,255,0) 100%)";
-    
+
     rohstoffe.style.background = "rgb(187,63,150)";
     rohstoffe.style.background = "linear-gradient(0deg, rgba(139,55,179,0.6) 0%, rgba(187,63,150,0.6) 100%)";
 
@@ -117,10 +113,10 @@ alles.onclick = ()=>{
 
 }
 
-rohstoffe.onclick = ()=>{
+rohstoffe.onclick = () => {
     rohstoffe.style.background = "rgba(255, 255, 255, 0.301)";
     rohstoffe.style.background = "linear-gradient(0deg, rgba(255,255,255,0.2) 0%, rgba(255,255,255,0.4) 35%, rgba(255,255,255,0.4) 72%, rgba(255,255,255,0) 100%)";
-    
+
     alles.style.background = "rgb(187,63,150)";
     alles.style.background = "linear-gradient(0deg, rgba(139,55,179,0.6) 0%, rgba(187,63,150,0.6) 100%)";
 
@@ -147,10 +143,10 @@ rohstoffe.onclick = ()=>{
 
 }
 
-arzneimittel.onclick = ()=>{
+arzneimittel.onclick = () => {
     arzneimittel.style.background = "rgba(255, 255, 255, 0.301)";
     arzneimittel.style.background = "linear-gradient(0deg, rgba(255,255,255,0.2) 0%, rgba(255,255,255,0.4) 35%, rgba(255,255,255,0.4) 72%, rgba(255,255,255,0) 100%)";
-    
+
     rohstoffe.style.background = "rgb(187,63,150)";
     rohstoffe.style.background = "linear-gradient(0deg, rgba(139,55,179,0.6) 0%, rgba(187,63,150,0.6) 100%)";
 
@@ -177,10 +173,10 @@ arzneimittel.onclick = ()=>{
 
 }
 
-essen.onclick = ()=>{
+essen.onclick = () => {
     essen.style.background = "rgba(255, 255, 255, 0.301)";
     essen.style.background = "linear-gradient(0deg, rgba(255,255,255,0.2) 0%, rgba(255,255,255,0.4) 35%, rgba(255,255,255,0.4) 72%, rgba(255,255,255,0) 100%)";
-    
+
     rohstoffe.style.background = "rgb(187,63,150)";
     rohstoffe.style.background = "linear-gradient(0deg, rgba(139,55,179,0.6) 0%, rgba(187,63,150,0.6) 100%)";
 
@@ -207,10 +203,10 @@ essen.onclick = ()=>{
 
 }
 
-waffen.onclick = ()=>{
+waffen.onclick = () => {
     waffen.style.background = "rgba(255, 255, 255, 0.301)";
     waffen.style.background = "linear-gradient(0deg, rgba(255,255,255,0.2) 0%, rgba(255,255,255,0.4) 35%, rgba(255,255,255,0.4) 72%, rgba(255,255,255,0) 100%)";
-    
+
     rohstoffe.style.background = "rgb(187,63,150)";
     rohstoffe.style.background = "linear-gradient(0deg, rgba(139,55,179,0.6) 0%, rgba(187,63,150,0.6) 100%)";
 
@@ -237,10 +233,10 @@ waffen.onclick = ()=>{
 
 }
 
-drogen.onclick = ()=>{
+drogen.onclick = () => {
     drogen.style.background = "rgba(255, 255, 255, 0.301)";
     drogen.style.background = "linear-gradient(0deg, rgba(255,255,255,0.2) 0%, rgba(255,255,255,0.4) 35%, rgba(255,255,255,0.4) 72%, rgba(255,255,255,0) 100%)";
-    
+
     rohstoffe.style.background = "rgb(187,63,150)";
     rohstoffe.style.background = "linear-gradient(0deg, rgba(139,55,179,0.6) 0%, rgba(187,63,150,0.6) 100%)";
 
@@ -267,10 +263,10 @@ drogen.onclick = ()=>{
 
 }
 
-trinken.onclick = ()=>{
+trinken.onclick = () => {
     trinken.style.background = "rgba(255, 255, 255, 0.301)";
     trinken.style.background = "linear-gradient(0deg, rgba(255,255,255,0.2) 0%, rgba(255,255,255,0.4) 35%, rgba(255,255,255,0.4) 72%, rgba(255,255,255,0) 100%)";
-    
+
     rohstoffe.style.background = "rgb(187,63,150)";
     rohstoffe.style.background = "linear-gradient(0deg, rgba(139,55,179,0.6) 0%, rgba(187,63,150,0.6) 100%)";
 
@@ -297,10 +293,10 @@ trinken.onclick = ()=>{
 
 }
 
-munition.onclick = ()=>{
+munition.onclick = () => {
     munition.style.background = "rgba(255, 255, 255, 0.301)";
     munition.style.background = "linear-gradient(0deg, rgba(255,255,255,0.2) 0%, rgba(255,255,255,0.4) 35%, rgba(255,255,255,0.4) 72%, rgba(255,255,255,0) 100%)";
-    
+
     rohstoffe.style.background = "rgb(187,63,150)";
     rohstoffe.style.background = "linear-gradient(0deg, rgba(139,55,179,0.6) 0%, rgba(187,63,150,0.6) 100%)";
 
@@ -327,10 +323,10 @@ munition.onclick = ()=>{
 
 }
 
-sonstiges.onclick = ()=>{
+sonstiges.onclick = () => {
     sonstiges.style.background = "rgba(255, 255, 255, 0.301)";
     sonstiges.style.background = "linear-gradient(0deg, rgba(255,255,255,0.2) 0%, rgba(255,255,255,0.4) 35%, rgba(255,255,255,0.4) 72%, rgba(255,255,255,0) 100%)";
-    
+
     rohstoffe.style.background = "rgb(187,63,150)";
     rohstoffe.style.background = "linear-gradient(0deg, rgba(139,55,179,0.6) 0%, rgba(187,63,150,0.6) 100%)";
 
@@ -361,24 +357,24 @@ sonstiges.onclick = ()=>{
 
 function allowDrop(ev) {
     ev.preventDefault();
-  }
-  
-  function drag(ev) {
+}
+
+function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
+}
+
+function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-  }
+}
 
-  function dropp(ev) {
-    
-    //var a = document.getElementsByClassName("rd_item rd_item1");
-    //document.getElementById('bodyitems').childNodes[1].childNodes[1].style.display = "Hut";
-    // document.getElementById('bodyitems').childNodes[1].childNodes[2].style.display = "none";
-    //ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
+// function dropp(ev) {
+
+//     //var a = document.getElementsByClassName("rd_item rd_item1");
+//     //document.getElementById('bodyitems').childNodes[1].childNodes[1].style.display = "Hut";
+//     // document.getElementById('bodyitems').childNodes[1].childNodes[2].style.display = "none";
+//     //ev.preventDefault();
+//     var data = ev.dataTransfer.getData("text");
+//     ev.target.appendChild(document.getElementById(data));
+// }
