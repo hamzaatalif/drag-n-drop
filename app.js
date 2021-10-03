9851a8a26117b5a6c21d1f795a3b5eab73a92069
hamzaatalif <hamzaatalif@gmail.com>
2021-10-03 15:00:24 +0500

    box shows color 2.1

--- app.js
+++ app.js
@@ -10,9 +10,33 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-    let ele = document.getElementById(data);
 }
 
 function noAllowDrop(ev) {
     ev.stopPropagation();
+}
+
+
+
+function dropCheck(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    let ele = document.getElementById(data);
+    ele.parentElement.previousElementSibling.style.background = "green"
+}
+
+
+setInterval(checkData,10)
+
+function checkData() {    
+    let dropBox = document.querySelectorAll(".droppedItemBox");
+    dropBox.forEach((box)=>{
+        if (!box.children.length){
+            box.previousElementSibling.style.background = "red"
+        }
+        else {
+            box.previousElementSibling.style.background = "green"
+        }
+    })
 }
