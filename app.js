a687542e49d461896a00bf1e7bec2884eacf8b6e
hamzaatalif <hamzaatalif@gmail.com>
2021-10-04 16:03:29 +0500

    90% done

--- app.js
+++ app.js
@@ -34,9 +34,37 @@ function checkData() {
     dropBox.forEach((box)=>{
         if (!box.children.length){
             box.previousElementSibling.style.background = "red"
+            box.style.backgroundImage = "url(./images/bg2.png)";
         }
         else {
-            box.previousElementSibling.style.background = "green"
+            box.previousElementSibling.style.background = "green"  
+            box.style.backgroundImage = "";
         }
     })
 }
+
+
+let menuItemsContainers = document.querySelectorAll(".menu-items-container")
+let menuBtns = document.querySelectorAll(".menu-btn");
+
+menuBtns.forEach((btn,index)=>{
+    btn.addEventListener("click",()=>{
+        let position = index;
+        menuBtns.forEach((btn,index)=>{
+            if (index==position) {
+                btn.classList.add("active-btn")
+            }
+            else {
+                btn.classList.remove("active-btn")
+            } 
+        })
+        menuItemsContainers.forEach((container,index)=>{            
+            if (index==position) {
+                container.classList.remove("not-active")
+            }
+            else {
+                container.classList.add("not-active")
+            }            
+        })
+    })
+})
